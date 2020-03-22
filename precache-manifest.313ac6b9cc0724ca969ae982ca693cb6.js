22c1c03dacea6c8673ba881a59bae0e3ccbfdf39
Christian Dallago <code@dallago.us>
2020-03-22 16:22:25 +0100

    Updates

---
+++ precache-manifest.313ac6b9cc0724ca969ae982ca693cb6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2893475bda9df02e64836c420f5403a3",
+    "url": "/bed_management_frontend/index.html"
+  },
+  {
+    "revision": "9c27e6920110cb3f7431",
+    "url": "/bed_management_frontend/static/css/2.bc68a1bb.chunk.css"
+  },
+  {
+    "revision": "f108b8fa9afed104afc4",
+    "url": "/bed_management_frontend/static/css/main.11566036.chunk.css"
+  },
+  {
+    "revision": "9c27e6920110cb3f7431",
+    "url": "/bed_management_frontend/static/js/2.b7f43914.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/bed_management_frontend/static/js/2.b7f43914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f108b8fa9afed104afc4",
+    "url": "/bed_management_frontend/static/js/main.f4baec2e.chunk.js"
+  },
+  {
+    "revision": "a8ce46c81f135509eaf0",
+    "url": "/bed_management_frontend/static/js/runtime-main.2089ea21.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/bed_management_frontend/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/bed_management_frontend/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/bed_management_frontend/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/bed_management_frontend/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/bed_management_frontend/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/bed_management_frontend/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
